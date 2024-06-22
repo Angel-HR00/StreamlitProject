536aff2f7c01636dd6769277d934d2fc810bc24f
Angel-HR00 <herreroangel11@gmail.com>
2024-06-22 16:29:41 +0200

    Proyecto temporalidad BTC/USD

--- src/app.py
+++ src/app.py
@@ -17,14 +17,14 @@ return_value = st.sidebar.selectbox(
 )
 
 #Preparar data
-@st.cache(ignore_hash = True)
+@st.cache
 def load_data():
     
     #Cargar dataset
     df = pd.read_csv("data/processed/data.csv")
     
     #Limpiar valores nulos
-    df. ta.utils.dropna(df)
+    df = ta.utils.dropna(df)
     
     df = ta.add_volatility_ta(df, "High", "Low", "Close", fillna=False, colprefix=ta_col_prefix)
     df = ta.add_momentum_ta(df, "High", "Low", "Close", "Volume_Currency", fillna=False, colprefix=ta_col_prefix)
@@ -36,13 +36,13 @@ df = load_data()
 df['y'] = (df['Close'] / df['Close'].shift(return_value) - 1) * 100
 
 #Limpiar valores nulos
-df =df.dropna(df)
+clean_df = ta.utils.dropna(df)
 
 #Streamlit
 st.title("EDA para datos financieros")
 
-a = datetime.utcfromtimestamp(df['Timestamp'].head(1)).strftime('%Y-%m-%d %H:%M:%S')
-b = datetime.utcfromtimestamp(df['Timestamp'].tail(1)).strftime('%Y-%m-%d %H:%M:%S')
+a = datetime.utcfromtimestamp(clean_df['Timestamp'].head(1)).strftime('%Y-%m-%d %H:%M:%S')
+b = datetime.utcfromtimestamp(clean_df['Timestamp'].tail(1)).strftime('%Y-%m-%d %H:%M:%S')
 
 st.write(f"Intentamos explorar un pequeño lapso de tiempo con precios BTC/USD desde {a} hasta {b}")
 st.write('')
@@ -53,26 +53,30 @@ st.write(df)
 st.subheader('Describe dataframe')
 st.write(df.describe())
 
-st.write('Number of rows: {}, Number of columns: {}'.format(*df.shape))
+st.write('Number of rows: {}, Number of columns: {}'.format(*clean_df.shape))
 
 st.subheader('Price')
 st.line_chart(df['Close'], width=max_width)
 
 st.subheader(f'Return {return_value} periods')
-st.area_chart(df['y'], width=max_width)
+st.area_chart(clean_df['y'], width=max_width)
 
 st.subheader('Target Histogram')
 bins = list(np.arange(-10, 10, 0.5))
-hist_values, hist_indexes = np.histogram(df['y'], bins=bins)
+hist_values, hist_indexes = np.histogram(clean_df['y'], bins=bins)
 st.bar_chart(pd.DataFrame(data=hist_values, index=hist_indexes[0:-1]), width=max_width)
 st.write('Target value min: {0:.2f}%; max: {1:.2f}%; mean: {2:.2f}%; std: {3:.2f}'.format(
-    np.min(df['y']), np.max(df['y']), np.mean(df['y']), np.std(df['y'])))
+    np.min(clean_df['y']), 
+    np.max(clean_df['y']), 
+    np.mean(clean_df['y']), 
+    np.std(clean_df['y'])
+    ))
 
 # Analisis univariante
 st.subheader('Correlation coefficient ta features and target column')
 
-x_cols = [col for col in df.columns if col not in ['Timestamp', 'y'] and col.startswith(ta_col_prefix)]
+x_cols = [col for col in clean_df.columns if col not in ['Timestamp', 'y'] and col.startswith(ta_col_prefix)]
 labels = [col for col in x_cols]
-values = [np.corrcoef(df[col], df['y'])[0, 1] for col in x_cols]
+values = [np.corrcoef(clean_df[col], clean_df['y'])[0, 1] for col in x_cols]
 
 st.bar_chart(data=pd.DataFrame(data=values, index=labels), width=max_width)
